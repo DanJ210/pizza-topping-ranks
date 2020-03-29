b88d17de6356f98a4fa1a94e00903fe9d8ecb0df
Daniel Jackson <danj210@hotmail.com>
2020-03-29 16:37:34 -0400

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaToppingRanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
